f75719a77bcaef490be20737f357031697b409bb
azu <azuciao@gmail.com>
2015-11-20 13:23:43 +0900

    add grun gulp

--- grunt-gulp.js
+++ grunt-gulp.js
@@ -4,7 +4,7 @@ var JSerStat = require("jser-stat").JSerStat;
 var countByGroup = require("jser-stat").compute.countByGroup;
 var stat = new JSerStat();
 
-console.log("年月\tBower\tnpm");
+console.log("年月\Grunt\tgulp");
 for (var i = 0; i < 12; i++) {
     var beginDate = new Date(2014, i, 1);
     var endDate = new Date(2014, i + 1, 1);
@@ -25,15 +25,15 @@ function con(beginDate, endDate) {
         var npm = /npm/i;
         var bower = /bower/i;
         if (bower.test(item.title) || bower.test(item.content)) {
-            rank["Bower"] = ++rank["Bower"] || 1;
+            rank["Grunt"] = ++rank["Grunt"] || 1;
         }
         if (npm.test(item.title) || npm.test(item.content)) {
-            rank["npm"] = ++rank["npm"] || 1;
+            rank["gulp"] = ++rank["gulp"] || 1;
         }
     });
     //process.stdout.write("#" + weeks[0].weekNumber + " - #" + weeks[weeks.length - 1].weekNumber + ", ");
     process.stdout.write(beginDate.getFullYear() + "年" + (beginDate.getMonth() + 1) + "月" + "\t ");
-    process.stdout.write(String((rank["Bower"] || 0)) + "\t");
-    process.stdout.write(String((rank["npm"] || 0)) + "\t");
+    process.stdout.write(String((rank["Grunt"] || 0)) + "\t");
+    process.stdout.write(String((rank["gulp"] || 0)) + "\t");
     console.log("");
 }
